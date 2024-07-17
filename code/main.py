7062b632e5e4dff538bea69575b7ec9151dc1672
Gurleen Kaur <gurleenkaurkhurpa19@gmail.com>
2024-07-16 21:01:36 -0400

    added background

--- code/main.py
+++ code/main.py
@@ -1,5 +1,8 @@
 from settings import *
 from player import Player
+from sprites import *
+from random import randint
+from pytmx.util_pygame import load_pygame
 
 class Game:
     def __init__(self):
@@ -12,8 +15,25 @@ class Game:
         
         #groups
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
+
+        self.setup()
+
         #sprites
-        self.player = Player((400,300), self.all_sprites)
+        self.player = Player((500,300), self.all_sprites, self.collision_sprites)
+    def setup(self):
+        map = load_pygame(join('data','maps', 'world.tmx'))
+
+        for x,y, image in map.get_layer_by_name('Ground').tiles():
+            Sprite((x * TILE_SIZE,y * TILE_SIZE),image, self.all_sprites)
+
+        for obj in map.get_layer_by_name('Objects'):
+            CollisionSprite((obj.x, obj.y),obj.image, (self.all_sprites, self.collision_sprites))
+        
+        for obj in map.get_layer_by_name('Collision'):
+            CollisionSprite((obj.x, obj.y),pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+    
+        
 
     def run(self):
         while self.running:
@@ -33,6 +53,7 @@ class Game:
             pygame.display.update()
 
         pygame.quit()
+
 if __name__ == "__main__":
     game = Game()
     game.run()
